72a41794fc64751868cecce1140244ee0b7c5012
Unknown <janc11832@gmail.com>
2018-10-27 19:56:23 -0400

    Josh's files

---
+++ demo_mongodb_findone.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  
+  dbo.createCollection("customers", function(err, res) {
+
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+  dbo.collection("customers").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    db.close();
+  });
+});
---
+++ demo_test.js
@@ -0,0 +1,23 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("beards", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+
+function postPic(){
+    var s = document.getElementById("pic");
+    document.getElementById("thePics").innerHTML = s;
+}
